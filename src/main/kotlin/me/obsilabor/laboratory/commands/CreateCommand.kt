3edb88e63700a3c458877c82e6d4318994cd6c30
mooziii <63669478+mooziii@users.noreply.github.com>
2022-07-26 22:44:15 +0200

    Update CreateCommand.kt

--- src/main/kotlin/me/obsilabor/laboratory/commands/CreateCommand.kt
+++ src/main/kotlin/me/obsilabor/laboratory/commands/CreateCommand.kt
@@ -6,20 +6,28 @@ import com.github.ajalt.clikt.parameters.arguments.default
 import com.github.ajalt.clikt.parameters.types.choice
 import kotlinx.coroutines.launch
 import me.obsilabor.laboratory.arch.Architecture
+import me.obsilabor.laboratory.arch.Server
 import me.obsilabor.laboratory.mainScope
+import me.obsilabor.laboratory.platform.PlatformResolver
 import me.obsilabor.laboratory.platform.impl.PaperPlatform
 import me.obsilabor.laboratory.terminal
 import me.obsilabor.laboratory.terminal.SpinnerAnimation
+import java.util.Random
 import kotlin.io.path.absolute
 
 class CreateCommand : CliktCommand(
     name = "create",
     help = "Creates a new server"
 ) {
+    private val name by argument(
+        "name",
+        help = "The name of this server"
+    )
+
     private val software by argument(
         "software",
         help = "The server-software that should be used for the new server"
-    ).choice(ignoreCase = true, choices = arrayOf("papermc"))
+    ).choice(ignoreCase = true, choices = arrayOf("papermc", "quiltmc", "fabricmc"))
 
     private val version by argument(
         "version",
@@ -33,10 +41,7 @@ class CreateCommand : CliktCommand(
 
     override fun run() {
         mainScope.launch {
-            val platform = when(software) {
-                "papermc" -> PaperPlatform
-                else -> throw RuntimeException("Unknown platform $software")
-            }
+            val platform = PlatformResolver.resolvePlatform(software)
             val spinner = SpinnerAnimation("Resolving minecraft versions for ${platform.name}")
             spinner.start()
             var chosenVersion = version
@@ -49,8 +54,18 @@ class CreateCommand : CliktCommand(
                 chosenBuild = platform.getBuilds(chosenVersion).last()
             }
             spinner.stop()
-            val jarFile = Architecture.findOrCreateJar(platform, chosenVersion, chosenBuild)
-            terminal.println(jarFile.absolute())
+            val server = Server(
+                Random().nextInt(700000), // TODO: lookup database to avoid duplicates
+                name,
+                true,
+                true,
+                emptyList(),
+                software,
+                chosenBuild,
+                chosenVersion,
+                true
+            )
+            server.start()
         }
     }
 }
