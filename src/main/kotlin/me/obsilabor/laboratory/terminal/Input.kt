da0f8630c73c779bee4c6ae8648ce40399c8c409
mooziii <63669478+mooziii@users.noreply.github.com>
2022-07-27 13:06:27 +0200

    Update Input.kt

--- src/main/kotlin/me/obsilabor/laboratory/terminal/Input.kt
+++ src/main/kotlin/me/obsilabor/laboratory/terminal/Input.kt
@@ -44,3 +44,35 @@ fun Terminal.awaitMemoryInput(
     }
     return amount
 }
+
+fun <T> Terminal.choose(
+    message: String,
+    entries: List<Pair<T, String>>,
+): T? {
+    entries.forEachIndexed { index, (_, string) ->
+        print("${index + 1}) ")
+        println(string)
+    }
+    while (true) {
+        print("$message (enter the number): ")
+        val input = readlnOrNull() ?: return null
+
+        if (input.isEmpty()) {
+            warning("Please enter a number")
+            continue
+        }
+
+        val index = input.toIntOrNull()?.minus(1)
+        if (index == null) {
+            warning("'$input' isn't a number")
+            continue
+        }
+
+        if (index !in entries.indices) {
+            warning("No such choice: '$input'")
+            continue
+        }
+
+        return entries.getOrNull(index)?.first
+    }
+}
