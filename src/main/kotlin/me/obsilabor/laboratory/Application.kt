080373bbec187d273419cb1b936f1ce53e8cb92a
mooziii <63669478+mooziii@users.noreply.github.com>
2022-07-27 11:18:42 +0200

    Update Application.kt

--- src/main/kotlin/me/obsilabor/laboratory/Application.kt
+++ src/main/kotlin/me/obsilabor/laboratory/Application.kt
@@ -1,13 +1,20 @@
 package me.obsilabor.laboratory
 
 import com.github.ajalt.mordant.rendering.TextColors
+import com.github.ajalt.mordant.rendering.TextStyles
 import kotlinx.coroutines.coroutineScope
 import me.obsilabor.laboratory.arch.Architecture
 import me.obsilabor.laboratory.commands.LaboratoryCommand
+import me.obsilabor.laboratory.utils.OperatingSystem
+import java.awt.Desktop
 
 suspend fun main(args: Array<String>) {
     if (System.getProperty("user.name").equals("root", true)) {
         terminal.println(TextColors.brightRed("Do NOT run laboratory as root!"))
+        return
+    }
+    if (!OperatingSystem.notWindows && !Desktop.isDesktopSupported()) {
+        terminal.println(TextStyles.bold(TextColors.brightRed("Do NOT run laboratory on a headless windows environment")))
         return
     }
     coroutineScope {
