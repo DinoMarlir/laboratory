f18515174d33227207dde6e93280a4b19385f12d
mooziii <63669478+mooziii@users.noreply.github.com>
2022-07-27 11:18:44 +0200

    Update Server.kt

--- src/main/kotlin/me/obsilabor/laboratory/arch/Server.kt
+++ src/main/kotlin/me/obsilabor/laboratory/arch/Server.kt
@@ -1,27 +1,37 @@
 package me.obsilabor.laboratory.arch
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import com.github.ajalt.mordant.rendering.TextColors
+import com.github.ajalt.mordant.rendering.TextStyles
+import kotlinx.coroutines.*
 import kotlinx.serialization.Serializable
 import me.obsilabor.laboratory.platform.PlatformResolver
+import me.obsilabor.laboratory.terminal
+import me.obsilabor.laboratory.utils.OperatingSystem
 import me.obsilabor.laboratory.utils.getDirectory
 import me.obsilabor.laboratory.utils.getFile
-import java.io.File
+import java.awt.Dimension
+import java.awt.event.WindowAdapter
+import java.awt.event.WindowEvent
 import java.nio.file.Files
 import java.nio.file.Path
+import javax.swing.JFrame
+import javax.swing.JLabel
+import javax.swing.WindowConstants
 
 @Serializable
 data class Server(
     val id: Int,
     var name: String,
-    var static: Boolean = true,
-    var copyTemplates: Boolean = true,
-    val templates: List<String> = emptyList(),
+    var static: Boolean,
+    var copyTemplates: Boolean,
+    val templates: List<String>,
     var platform: String,
     var platformBuild: String,
     var mcVersion: String,
-    var automaticUpdates: Boolean = true,
-    var maxHeapMemory: Long = 1024
+    var automaticUpdates: Boolean,
+    var maxHeapMemory: Long,
+    var jvmArguments: List<String>,
+    var processArguments: List<String>
 ) {
 
     val directory by lazy { getDirectory(Architecture.Servers, "$name-$id") }
@@ -39,9 +49,49 @@ data class Server(
                 # By using laboratory you automatically agree to the Mojang and Laboratory Terms of Service
                 eula=true
             """.trimIndent())
-            val process = ProcessBuilder("java -Xmx${maxHeapMemory}M", "-jar", "server.jar").directory(directory).redirectErrorStream(true).redirectOutput(
-                ProcessBuilder.Redirect.INHERIT)
-            process.start()
+        }
+        if (OperatingSystem.notWindows) {
+            val args = arrayListOf(
+                "screen",
+                "-dmS",
+                "$name-$id",
+                "java",
+                "-Xmx${maxHeapMemory}M",
+            )
+            args.addAll(jvmArguments)
+            args.add("-jar")
+            args.add("server.jar")
+            args.addAll(processArguments)
+            val process = ProcessBuilder(args).directory(directory).start()
+            terminal.println("Server is now running with PID ${process.pid()}. Attach using ${TextStyles.dim(TextColors.brightWhite("screen -dr $name-$id"))}")
+        } else {
+            terminal.println("Server is now running with PID windows-moment. Attach using ${TextStyles.dim(TextColors.brightWhite("screen -dr $name-$id"))}")
+            val args = arrayListOf(
+                "java",
+                "-Xmx${maxHeapMemory}M",
+            )
+            args.addAll(jvmArguments)
+            args.add("-jar")
+            args.add("server.jar")
+            args.addAll(processArguments)
+            val process = ProcessBuilder(
+                args,
+            )
+                .directory(directory)
+                .redirectErrorStream(true)
+                .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+                .redirectInput(ProcessBuilder.Redirect.INHERIT)
+                .start()
+            val frame = JFrame("Windows is for development purposes only!")
+            frame.add(JLabel("Windows doesn't support screen and shouldn't be used in production. Closing this window will result in the process being terminated."))
+            frame.addWindowListener(object : WindowAdapter() {
+                override fun windowClosing(e: WindowEvent) {
+                    process.destroyForcibly()
+                }
+            })
+            frame.defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+            frame.isVisible = true
+            frame.size = Dimension(780, 80)
         }
     }
 }
