b1c41a937f2a1f5afd54f0925c8e64c487a8da01
mooziii <63669478+mooziii@users.noreply.github.com>
2022-07-29 15:43:53 +0200

    Update ServerCommand.kt

--- src/main/kotlin/me/obsilabor/laboratory/commands/ServerCommand.kt
+++ src/main/kotlin/me/obsilabor/laboratory/commands/ServerCommand.kt
@@ -3,6 +3,7 @@ package me.obsilabor.laboratory.commands
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.subcommands
 import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.arguments.optional
 import com.github.ajalt.mordant.rendering.TextColors
 import kotlinx.coroutines.launch
 import me.obsilabor.laboratory.db.JsonDatabase
@@ -28,10 +29,10 @@ class ServerCommand : CliktCommand(
         name = "modify",
         help = "Modify a server"
     ) {
-        private val query by argument("query", help = "Name or id of the server to modify")
+        private val query by argument("query", help = "Name or id of the server to modify").optional()
 
         override fun run() {
-            val server = terminal.chooseServer(query) ?: return
+            val server = terminal.chooseServer(query ?: "") ?: return
             val action = terminal.choose("What do you want to do?", ServerEditAction.values().map {
                 it to it.actionString
             }) ?: return
@@ -43,10 +44,10 @@ class ServerCommand : CliktCommand(
         name = "delete",
         help = "Deletes a server"
     ) {
-        private val query by argument("query", help = "Name or id of the server to delete")
+        private val query by argument("query", help = "Name or id of the server to delete").optional()
 
         override fun run() {
-            val server = terminal.chooseServer(query) ?: return
+            val server = terminal.chooseServer(query ?: "") ?: return
             if (server != null) {
                 if (terminal.promptYesOrNo(TextColors.brightRed("The server ${server.name} and all its content will be deleted. Are you sure about that?"), default = false)) {
                     mainScope.launch {
