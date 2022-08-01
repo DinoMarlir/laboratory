cdf3fb47bd1c4528148a8d02165e35258be6701a
mooziii <63669478+mooziii@users.noreply.github.com>
2022-08-01 13:10:18 +0200

    Update InfoCommand.kt

--- src/main/kotlin/me/obsilabor/laboratory/commands/InfoCommand.kt
+++ src/main/kotlin/me/obsilabor/laboratory/commands/InfoCommand.kt
@@ -2,7 +2,12 @@ package me.obsilabor.laboratory.commands
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.mordant.rendering.TextColors
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import kotlinx.coroutines.launch
 import me.obsilabor.laboratory.VERSION
+import me.obsilabor.laboratory.httpClient
+import me.obsilabor.laboratory.mainScope
 import me.obsilabor.laboratory.terminal
 import me.obsilabor.laboratory.terminal.SpinnerAnimation
 
@@ -12,7 +17,14 @@ class InfoCommand : CliktCommand(
 ) {
     override fun run() {
         terminal.println("Laboratory version: ${TextColors.brightCyan(VERSION)}")
-        val spinner = SpinnerAnimation("Resolving latest version")
-        val latest = 
+        mainScope.launch {
+            runCatching {
+                val latest = httpClient.get("https://raw.githubusercontent.com/mooziii/laboratory/main/.version").bodyAsText()
+                terminal.println("Newest version: ${TextColors.brightGreen(latest)}")
+                if (latest != VERSION) {
+                    terminal.println(TextColors.brightRed("Your version doesn't match the newest one. Please update laboratory."))
+                }
+            }.onFailure {}
+        }
     }
 }
