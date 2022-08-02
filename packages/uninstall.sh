def453997eb524b143bdca070a9218f3a82d3592
DinoMarlir <72864666+DinoMarlir@users.noreply.github.com>
2022-08-02 10:15:53 +0200

    Update uninstall.sh [skip ci]

--- packages/uninstall.sh
+++ packages/uninstall.sh
@@ -3,8 +3,7 @@
 #delete servers
 #https://stackoverflow.com/questions/1885525/how-do-i-prompt-a-user-for-confirmation-in-bash-script
 
-read -p "Should all data and servers be deleted? (y/N) " -n 1 -r
-echo "~/laboratory" 
+read -p "Should all data and servers be deleted? [~/laboratory] (y/N) " -n 1 -r
 echo    
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
