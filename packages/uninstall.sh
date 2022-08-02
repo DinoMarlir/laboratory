9866ffb736246ba8551bc9d6acd7b99db8a99609
DinoMarlir <72864666+DinoMarlir@users.noreply.github.com>
2022-08-02 10:03:03 +0200

    Final update uninstall.sh [skip ci]

--- packages/uninstall.sh
+++ packages/uninstall.sh
@@ -1,2 +1,17 @@
-rm /usr/bin/laboratory
-rm -rf /usr/share/laboratory/
+#!/usr/bin/bash
+
+#delete servers
+#https://stackoverflow.com/questions/1885525/how-do-i-prompt-a-user-for-confirmation-in-bash-script
+
+read -p "Should all data and servers be deleted? (y/N) " -n 1 -r
+echo "~/laboratory" 
+echo    
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    rm -rf ~/laboratory
+fi 
+
+sudo rm /usr/bin/laboratory
+sudo rm -rf /usr/share/laboratory/
+
+echo "laboratory successfully uninstalled"
