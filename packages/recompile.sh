bd4d979f85c7c987489613dab88d4597f733bad0
mooz <63669478+mooziii@users.noreply.github.com>
2022-08-31 20:59:09 +0200

    hm [skip ci]

--- packages/recompile.sh
+++ packages/recompile.sh
@@ -2,6 +2,6 @@ git pull
 cd ..
 chmod +x gradlew
 ./gradlew distZip
-cp ../laboratory-cli/build/distributions/laboratory-cli-jvm.zip packages/laboratory-cli-jvm.zip
+cp laboratory-cli/build/distributions/laboratory-cli-jvm.zip packages/laboratory-cli-jvm.zip
 chmod +x install.sh
 sudo ./install.sh
