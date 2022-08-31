0778f0b4cd54e93c9ebbb83acbc927ab56134f40
mooz <63669478+mooziii@users.noreply.github.com>
2022-08-31 20:53:08 +0200

    Script fixes [skip ci]

--- packages/recompile.sh
+++ packages/recompile.sh
@@ -1,5 +1,5 @@
 git pull
 cd ..
 ./gradlew distZip
-cp ..laboratory-cli/build/distributions/laboratory-cli-jvm.zip laboratory-cli-jvm.zip
+cp ../laboratory-cli/build/distributions/laboratory-cli-jvm.zip laboratory-cli-jvm.zip
 sudo ./install.sh
