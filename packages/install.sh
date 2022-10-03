b90d9ef9fc171f3db6fbca6b6a9cf3be946b6615
mooz <63669478+mooziii@users.noreply.github.com>
2022-10-03 21:38:33 +0200

    Instantly source the completion script to deliver instant completion support

--- packages/install.sh
+++ packages/install.sh
@@ -11,10 +11,12 @@ then
     _LABORATORY_COMPLETE=bash laboratory > ~/laboratory/bash-completion.sh
     echo "#Tab completion for laboratory. Do not remove this" >> ~/.bashrc
     echo "source ~/laboratory/bash-completion.sh" >> ~/.bashrc
+    source ~/laboratory/bash-completion.sh
 fi
 if [[ $SHELL == *"zsh"* ]]
 then
     _LABORATORY_COMPLETE=bash laboratory > ~/laboratory/zsh-completion.sh
     echo "#Tab completion for laboratory. Do not remove this" >> ~/.zshrc
     echo "source ~/laboratory/zsh-completion.sh" >> ~/.zshrc
+    source ~/laboratory/zsh-completion.sh
 fi
