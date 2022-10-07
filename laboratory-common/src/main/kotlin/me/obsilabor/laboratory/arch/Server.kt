c75e12057aa90b75d64b3109dd83b4e487259253
mooz <63669478+mooziii@users.noreply.github.com>
2022-10-07 20:12:56 +0200

    wait for process ;d

--- laboratory-common/src/main/kotlin/me/obsilabor/laboratory/arch/Server.kt
+++ laboratory-common/src/main/kotlin/me/obsilabor/laboratory/arch/Server.kt
@@ -154,9 +154,13 @@ data class Server(
             if (!attach) {
                 val process = processBuilder.start()
                 val pid = process.pid()+2 // don't ask, pid of the process is actually 2 numbers higher
-                terminal.println("Server is now running with PID $pid. Attach using ${(TextColors.brightWhite on TextColors.gray)(TextStyles.italic("screen -dr $name-$id"))}")
                 this.pid = pid
                 JsonDatabase.editServer(this)
+                if (!disableIO) {
+                    terminal.println("Server is now running with PID $pid. Attach using ${(TextColors.brightWhite on TextColors.gray)(TextStyles.italic("screen -dr $name-$id"))}")
+                } else {
+                    process.waitFor()
+                }
             } else {
                 withContext(Dispatchers.Default) {
                     runBlocking {
