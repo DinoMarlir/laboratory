2187aa7b9182956ecfb3097c31fb70b6d26f11c8
mooz <63669478+mooziii@users.noreply.github.com>
2022-10-28 10:07:49 +0200

    Detect homebrew and scoop java installations

--- laboratory-common/src/main/kotlin/me/obsilabor/laboratory/utils/JavaUtils.kt
+++ laboratory-common/src/main/kotlin/me/obsilabor/laboratory/utils/JavaUtils.kt
@@ -18,17 +18,29 @@ fun searchForJavaInstallations(): List<File> {
             if (eclipseFoundationFolder.exists()) {
                 list.addAll(eclipseFoundationFolder.listFiles().filter { it.isDirectory && it.name.contains("jdk") })
             }
+            val scoopFolder = File(System.getProperty("user.home"), "scoop/apps")
+            if (scoopFolder.exists()) {
+                list.addAll(scoopFolder.listFiles().filter { it.isDirectory && it.name.lowercase().contains("jdk") })
+            }
         }
         OperatingSystem.LINUX -> {
             val usrLibJvmFolder = File("/usr/lib/jvm")
             if (usrLibJvmFolder.exists()) {
                 list.addAll(usrLibJvmFolder.listFiles().filter { it.isDirectory })
             }
+            val homebrewFolder = File("/home/linuxbrew/.linuxbrew/Cellar/")
+            if (homebrewFolder.exists()) {
+                list.addAll(homebrewFolder.listFiles().filter { it.isDirectory && it.name.lowercase().contains("jdk") })
+            }
         }
         OperatingSystem.MACOS -> {
             val macosJavaFolder = File("/Library/Java/JavaVirtualMachines")
             if (macosJavaFolder.exists()) {
                 list.addAll(macosJavaFolder.listFiles().filter { it.isDirectory })
+            }
+            val homebrewFolder = File("/usr/local/Cellar/")
+            if (homebrewFolder.exists()) {
+                list.addAll(homebrewFolder.listFiles().filter { it.isDirectory && it.name.lowercase().contains("jdk") })
             }
         }
         else -> {}
